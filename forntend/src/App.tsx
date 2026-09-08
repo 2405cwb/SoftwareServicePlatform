943a8ed53e08705caf3112554b3b309ddf6572d3
cwb <aa944589323@gmail.com>
2026-09-08 17:52:13 +0800

    feat:增加前端路由和侧边栏导航

--- forntend/src/App.tsx
+++ forntend/src/App.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import CustomerPage from "./pages/CustomerPage";
+import SoftwarePage from "./pages/SoftwarePage";
 
 function App() {
   return (
@@ -8,8 +10,21 @@ function App() {
       <div className="header">
         <h1>软件服务管理平台</h1>
       </div>
+      <div className="main-layout">
+        <div className="sidebar">
+          <Link to="/customers">客户管理</Link>
+          <Link to="/software">软件管理</Link>
+        </div>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/customers" replace />} />
 
-      <CustomerPage />
+            <Route path="/customers" element={<CustomerPage />} />
+
+            <Route path="/software" element={<SoftwarePage />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
